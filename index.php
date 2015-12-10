d37503b8fac00751e83669c80e3e28905812622d
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2015-12-10 13:57:34 +0000

    Changing index.php indentation

--- index.php
+++ index.php
@@ -6,19 +6,19 @@ require_once "include/trata_session.php";
 
 require_once "include/HTML_partecima.php";
 
-if($_SESSION['nameUser']!=""){
+if ($_SESSION['nameUser'] != "") {
 
-	if($_SESSION['partidasJogadas']==$totalPartidas){
+	if ($_SESSION['partidasJogadas'] == $totalPartidas) {
 
 		require_once "include/finaliza.php";
 
-	}else{
+	} else {
 	
 		require_once "include/calculadora.php";
 
 	}
 
-}else if($_SESSION['nameUser']=="" && $_SESSION['partidasJogadas']==0){
+} else if ($_SESSION['nameUser'] == "" && $_SESSION['partidasJogadas'] == 0) {
 
 	require_once "include/registro.php";
 
